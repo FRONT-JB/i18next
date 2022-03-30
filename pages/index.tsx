3e0b2c6265bb40476e28e0cbb5e829da4870711c
전병길 <jeonbg@kakao.com>
2022-03-30 22:16:35 +0900

    fix : index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,11 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   const { t } = useTranslation();
   const handleChange = (e: MouseEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget.name);
     i18next.changeLanguage(e.currentTarget.name);
   };
   return (
     <div className={styles.container}>
-      <p>{t("test")}</p>
+      <p>{t("이름")}</p>
       <button
         type="button"
         name="ko-KR"
